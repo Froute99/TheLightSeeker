9c33b94d0f514d534fb5b01e65760b44c43a772f
Froute99 <98374874+Froute99@users.noreply.github.com>
2023-09-21 10:41:30 +0900

    [GAS] GameAbility System Header Modified
    Functions in header should be implemented

---
+++ Source/TheLightSeeker/Actors/Characters/CharacterBase.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
+
+UCLASS()
+class THELIGHTSEEKER_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+/************************
+ * Game Abilities System
+ ************************/
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "LightSeeker|Abilities")
+		TWeakObjectPtr<class UCharacterAbilitySystemComponent> ASC;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "LightSeeker|Abilities")
+		TWeakObjectPtr<class UCharacterAttributeSetBase> AttributeSet;
+
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "LightSeeker|Abilities")
+		TArray<TSubclassOf<class UGameplayAbility>> CharacterAbilities;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LightSeeker|Abilities")
+		TSubclassOf<class UGameplayEffect> DefaultAttributes;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "LightSeeker|Abilities")
+		TArray<TSubclassOf<class UGameplayEffect>> StartupEffects;
+
+
+	UFUNCTION(BlueprintCallable, Category = "Character|Attributes")
+		float GetCharacterLevel() const;
+	UFUNCTION(BlueprintCallable, Category = "Character|Attributes")
+		float GetHealth() const;
+	UFUNCTION(BlueprintCallable, Category = "Character|Attributes")
+		float GetMaxHealth() const;
+	UFUNCTION(BlueprintCallable, Category = "Character|Attributes")
+		float GetStamina() const;
+	UFUNCTION(BlueprintCallable, Category = "Character|Attributes")
+		float GetMaxStamina() const;
+
+	virtual void OnRep_PlayerState() override;
+	void InitializeStartingValues(class APlayerState* PS);
+
+
+	virtual void AddCharacterAbilities();
+	virtual void InitializeAttributes();
+	virtual void AddStartupEffects();
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
--- Source/TheLightSeeker/TheLightSeeker.Build.cs
+++ Source/TheLightSeeker/TheLightSeeker.Build.cs
@@ -19,10 +19,18 @@ public class TheLightSeeker : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+
+		PublicIncludePaths.AddRange(new string[] {
+			"TheLightSeeker/",
+			"TheLightSeeker/Actors",
+
+		});
+
 	}
 }
