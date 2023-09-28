666cbd3c59ea1b2373ebff9338ff71e864b45aee
Haewon Shon <haewon8896@gmail.com>
2023-09-28 18:18:59 +0900

    [style] Character Mesh and Camera adjustment
    Applied character mesh
    adjusted camera position

--- Source/TheLightSeeker/Actors/Characters/CharacterBase.cpp
+++ Source/TheLightSeeker/Actors/Characters/CharacterBase.cpp
@@ -22,8 +22,6 @@ ACharacterBase::ACharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
-
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
 	SpringArm->SetupAttachment(GetCapsuleComponent());
 	SpringArm->TargetArmLength = 400.0f;
