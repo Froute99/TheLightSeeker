0e410df8e5db4e09b3afa136754e2d04cfffc08a
Haewon Shon <haewon8896@gmail.com>
2023-12-01 16:21:31 +0900

    [Impl] Dummy for test & test fire enchant ability

---
+++ Source/TheLightSeeker/GameAbilitySystem/GA_SelfBuffAbility.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameAbilitySystem/GA_SelfBuffAbility.h"
+#include "GameAbilitySystem/CharacterAbilitySystemComponent.h"
+
+void UGA_SelfBuffAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated buff ability"));
+	FGameplayEffectSpecHandle DamageEffectSpecHandle = MakeOutgoingGameplayEffectSpec(DamageGameplayEffect, GetAbilityLevel());
+	ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*DamageEffectSpecHandle.Data.Get());
+}
