8d723586d9c0e45892d087a983e1a77e4b1b50f9
Froute99 <98374874+Froute99@users.noreply.github.com>
2023-12-14 01:34:13 +0900

    [Impl] Targeting Projectile
    Character now can confirm/cancel the targeting ability.
    
    Skill tree node need a giving skill as variable

--- Source/TheLightSeeker/GameAbilitySystem/CharacterAnimAbility.h
+++ Source/TheLightSeeker/GameAbilitySystem/CharacterAnimAbility.h
@@ -23,20 +23,21 @@ public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 	UFUNCTION()
-		void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
+		virtual void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
 
 	UFUNCTION()
-		void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
+		virtual void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
 
 	UFUNCTION()
-		void EventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
+		virtual void EventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 		TSubclassOf<AActor> ArrowClass;
 
+
+	class UAT_PlayMontageAndWaitForEvent* TaskHandle;
 	UFUNCTION(BlueprintCallable)
 		void PlayAnim();
-	class UAT_PlayMontageAndWaitForEvent* TaskHandle;
 
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EventTags")
